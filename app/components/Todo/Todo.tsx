e8ab387abae907e3b7609c3eb69e71e71c2f799f
Misha <misha@novatori.ge>
2023-06-20 15:55:24 +0400

    asd

--- app/components/Todo/Todo.tsx
+++ app/components/Todo/Todo.tsx
@@ -5,7 +5,7 @@ export default () => {
     const [task, setTask] = useState('')
     const [tasks, setTasks] = useState(['ლექციაა გადასაღები', 'ალგორითმია დასამატებელი'])
 
-    const onChange = (e: any) => {
+    const onChange = (e) => {
         setTask(e.target.value)
     }
 
