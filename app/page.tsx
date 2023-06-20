1432427c9c2ae84ea2ac841fe270239fc10cee74
Misha <misha@novatori.ge>
2023-06-20 11:18:45 +0400

    fix issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 import styles from './page.module.css'
-<<<<<<< HEAD
-=======
 import Todo from './components/Todo/Todo'
->>>>>>> 6d37d4b (displayname)
 import Button from './components/Button/Button'
 
 export default function Home() {
@@ -15,14 +12,8 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-<<<<<<< HEAD
-
-      <div className={styles.center}>
-        <Button title='ავტორიზაცია' />
-=======
       <div className={styles.center}>
         <Button title='რეგისტრაცია' />
->>>>>>> 6d37d4b (displayname)
       </div>
     </main>
   )
