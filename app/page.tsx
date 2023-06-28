e1965f5fedb17129266b84d2bec4e5116b96383b
Misha <misha@novatori.ge>
2023-06-28 10:02:11 +0400

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,47 @@
 'use client'
+import axios from 'axios'
 import styles from './page.module.css'
-import Todo from './components/Todo/Todo'
-import Button from './components/Button/Button'
+import { useEffect, useState } from 'react'
+
 
 export default function Home() {
 
-  const onRegister = () => {
-    // ...........
-    alert('თქვენ წარმატებით გაიარეთ რეგისტრაცია!')
-  };
+  const [products, setProducts] = useState([])
+  const [sort, setSort] = useState('asc')
+  const [limit, setLimit] = useState(20)
+
+
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products?sort=${sort}&limit=${limit}`)
+      .then((result) => {        
+        setProducts(result.data);
+      })
+  }, [sort, limit]);
 
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <Button title='რეგისტრაცია' />
-        <h1>ასწორებს ავტომატური დიფლოიმენთი!!!!!</h1>
+        <select onChange={(e) => {
+          setSort(e.target.value)
+        }}>
+          <option>asc</option>
+          <option>desc</option>
+        </select>
+        <input type='number' 
+                value={limit} 
+                onChange={(e) => {
+                  setLimit(+e.target.value)
+                }} 
+          />
+        <ul>
+          {
+            products.map((product: any) => 
+            <li>
+              <img width='50px' src={product.image} />
+              {product.title}
+            </li>)
+          }
+        </ul>
       </div>
     </main>
   )
